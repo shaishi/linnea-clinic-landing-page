d43a17ad5d86e377212606672c5eec2320481a89
Shai Shilo <shai_shilo@Shais-MBP-2.lan>
2026-08-18 17:56:51 +0300

    Make Vite base path dynamic for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/linnea-clinic-landing-page/',
+  base: process.env.VERCEL ? '/' : './',
   build: {
     rollupOptions: {
       input: {
